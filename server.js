d91aaf8c4f3e54aa6c5fb5c8be4f3175d52a26e5
gmk-dev-user <drussell@growmark.com>
2018-10-08 14:37:17 -0500

    Trying to add integration test back in

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 80,
+  port = process.env.PORT || 8080,
   bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
--- test/integrationTest.js
+++ test/integrationTest.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 var request = require('request');
 
 describe('Test endpoint', function() {
-    var url = 'http://localhost';
+    var url = 'http://ec2-18-209-8-59.compute-1.amazonaws.com:8080';
 
     it('should return \'You made it!\'', (done) => {
         request(url, (error, response, body) => {
