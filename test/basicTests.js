ff824f77a1d762b03e06be3c64a933c67102a372
gmk-dev-user <drussell@growmark.com>
2018-10-08 09:44:10 -0500

    Changed test to mock HTTP calls

--- test/basicTests.js
+++ test/basicTests.js
@@ -1,13 +1,38 @@
 var assert = require('assert');
-var request = require('request');
+// var request = require('request');
+var controller = require('../api/controllers/mainController');
+var http_mocks = require('node-mocks-http');
+// var should = require('should');
 
-describe('Test endpoint', function() {
-    var url = 'http://localhost:3000/test';
+function buildResponse() {
+    return http_mocks.createResponse({eventEmitter: require('events').EventEmitter})
+}
 
+describe('Test controller testGet', function() {
     it('should return \'You made it!\'', (done) => {
-        request(url, (error, response, body) => {
+        var response = buildResponse();
+        var request = http_mocks.createRequest({
+            method: 'GET',
+            url: '/'
+        });
+
+        response.on('end', function() {
+            var body = response._getData();
             assert.equal(body, 'You made it!');
-            done();
-        } );
+            done()
+        });
+
+        controller.testGet(request, response);
     });
 });
+
+// describe('Test endpoint', function() {
+//     var url = 'http://localhost:3000/test';
+
+//     it('should return \'You made it!\'', (done) => {
+//         request(url, (error, response, body) => {
+//             assert.equal(body, 'You made it!');
+//             done();
+//         } );
+//     });
+// });
