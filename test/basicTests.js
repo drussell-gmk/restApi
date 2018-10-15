b545dd100834e80cf2d61d41d128c72f92788381
gmk-dev-user <drussell@growmark.com>
2018-10-15 10:15:17 -0500

    features all staged

--- test/basicTests.js
+++ test/basicTests.js
@@ -9,7 +9,7 @@ function buildResponse() {
 }
 
 describe('Test controller testGet', function() {
-    it('should return \'Change number 1000!\'', (done) => {
+    it('should return \'We just implemented a new feature!\'', (done) => {
         var response = buildResponse();
         var request = http_mocks.createRequest({
             method: 'GET',
@@ -18,7 +18,7 @@ describe('Test controller testGet', function() {
 
         response.on('end', function() {
             var body = response._getData();
-            assert.equal(body, 'Change number 1000!');
+            assert.equal(body, 'We just implemented a new feature!');
             done()
         });
 
