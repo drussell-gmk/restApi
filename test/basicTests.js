7e362256685f374ee837aef17c56b796b9c5bb67
gmk-dev-user <drussell@growmark.com>
2018-10-18 13:08:21 -0500

    changes

--- test/basicTests.js
+++ test/basicTests.js
@@ -7,7 +7,7 @@ function buildResponse() {
 }
 
 describe('Test controller testGet', function() {
-    it('should return \'finally getting it to work!\'', (done) => {
+    it('should return \'work work work!\'', (done) => {
         var response = buildResponse();
         var request = http_mocks.createRequest({
             method: 'GET',
@@ -16,7 +16,7 @@ describe('Test controller testGet', function() {
 
         response.on('end', function() {
             var body = response._getData();
-            assert.equal(body, 'finally getting it to work!');
+            assert.equal(body, 'work work work!');
             done()
         });
 
